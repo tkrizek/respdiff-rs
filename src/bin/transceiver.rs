7944da71c5b982046e2a9b6d1f3d2c57153d773e
Tomas Krizek <tomas.krizek@nic.cz>
2021-07-19 18:23:28 +0200

    transceiver: write metadb info (dirty PoC)

--- src/bin/transceiver.rs
+++ src/bin/transceiver.rs
@@ -2,9 +2,17 @@ extern crate lmdb;
 
 use std::error::Error;
 use std::path::Path;
+use std::str;
+use std::time::SystemTime;
+use std::convert::TryInto;
+
+use byteorder::{ByteOrder, LittleEndian};
+
 use lmdb::Environment;
 use lmdb::Transaction;
 use lmdb::Cursor;
+use lmdb::WriteFlags;
+use lmdb::DatabaseFlags;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let env = Environment::new()
@@ -14,15 +22,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         .open(Path::new("/tmp/respdiff-rs.db"))?;
 
     let db = env.open_db(Some("queries"))?;
-    let txn = env.begin_ro_txn()?;
+    {
+        let txn = env.begin_ro_txn()?;
 
-    let mut cur = txn.open_ro_cursor(db)?;
+        let mut cur = txn.open_ro_cursor(db)?;
 
-    cur.iter().for_each(|res| {
-        if let Ok((key, val)) = res {
-            println!("{:?} -> {:?}", key, val);
-        }
-    });
+        cur.iter().for_each(|res| {
+            if let Ok((key, val)) = res {
+                let key = LittleEndian::read_u32(&key);
+                //println!("{:?} -> {:?}", key, val);
+            }
+        });
+    }
+
+    let ascii: Vec<u8> = "meta".into();
+
+    let metadb = env.create_db(Some("meta"), DatabaseFlags::empty())?;
+    {
+        let ts: u32 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs().try_into().unwrap();
+        let mut ts_buf = [0; 4];
+        LittleEndian::write_u32(&mut ts_buf, ts);
+
+        let mut txn = env.begin_rw_txn()?;
+        txn.put(metadb, b"version", b"2018-05-22", WriteFlags::empty())?;
+        txn.put(metadb, b"start_time", &ts_buf, WriteFlags::empty())?;
+        txn.commit()?;
+    }
+    {
+        let txn = env.begin_ro_txn()?;
+        //let vers: String = txn.get(metadb, b"version")?.into();
+        let vers = txn.get(metadb, b"version")?;
+        let ts = txn.get(metadb, b"start_time")?;
+        println!("{:?} {:?}", vers, ts);
+        let vers = str::from_utf8(vers)?;
+        let ts = LittleEndian::read_u32(&ts);
+        println!("{} {}", vers, ts);
+    }
 
     Ok(())
 }
