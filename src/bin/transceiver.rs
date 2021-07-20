eef8a0fce43ff4b0471f491f0a9bc61ec9e886a7
Tomas Krizek <tomas.krizek@nic.cz>
2021-07-20 15:47:51 +0200

    transciever: refactor - use lib functions

--- src/bin/transceiver.rs
+++ src/bin/transceiver.rs
@@ -2,64 +2,54 @@ extern crate lmdb;
 
 use std::error::Error;
 use std::path::Path;
-use std::str;
-use std::time::SystemTime;
-use std::convert::TryInto;
-
-use byteorder::{ByteOrder, LittleEndian};
-
-use lmdb::Environment;
 use lmdb::Transaction;
-use lmdb::Cursor;
-use lmdb::WriteFlags;
-use lmdb::DatabaseFlags;
-
 use respdiff::database;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let dir = Path::new("/tmp/respdiff-rs.db");
-    let env = match database::open_env(&dir) {
+    let env = match database::open_env(dir) {
         Ok(env) => env,
-        Err(error) => panic!("Failed to open LMDB environment: {:?}", error),
+        Err(e) => panic!("Failed to open LMDB environment: {:?}", e)
     };
+    let metadb = match database::open_db(&env, &database::metadb::NAME, true) {
+        Ok(db) => db,
+        Err(e) => panic!(
+            "Failed to open LMDB database '{}': {:?}",
+            &database::metadb::NAME,
+            e)
+    };
+    //let querydb = match database::open_db(&env, &database::querydb::NAME, true) {
+    //    Ok(db) => db,
+    //    Err(e) => panic!(
+    //        "Failed to open LMDB database '{s}': {:?}",
+    //        &database::querydb::NAME,
+    //        e);
+    //}
 
-    let db = env.open_db(Some("queries"))?;
+    //{
+    //    let txn = db.begin_ro_txn()?;
+
+    //    let mut cur = txn.open_ro_cursor(db)?;
+
+    //    cur.iter().for_each(|res| {
+    //        if let Ok((key, val)) = res {
+    //            let key = LittleEndian::read_u32(&key);
+    //            //println!("{:?} -> {:?}", key, val);
+    //        }
+    //    });
+    //}
+
     {
-        let txn = env.begin_ro_txn()?;
-
-        let mut cur = txn.open_ro_cursor(db)?;
-
-        cur.iter().for_each(|res| {
-            if let Ok((key, val)) = res {
-                let key = LittleEndian::read_u32(&key);
-                //println!("{:?} -> {:?}", key, val);
-            }
-        });
-    }
-
-    let ascii: Vec<u8> = "meta".into();
-
-    let metadb = env.create_db(Some("meta"), DatabaseFlags::empty())?;
-    {
-        let ts: u32 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs().try_into().unwrap();
-        let mut ts_buf = [0; 4];
-        LittleEndian::write_u32(&mut ts_buf, ts);
-
         let mut txn = env.begin_rw_txn()?;
-        txn.put(metadb, b"version", b"2018-05-22", WriteFlags::empty())?;
-        txn.put(metadb, b"start_time", &ts_buf, WriteFlags::empty())?;
+        database::metadb::write_version(metadb, &mut txn)?;
+        database::metadb::write_start_time(metadb, &mut txn)?;
         txn.commit()?;
     }
     {
         let txn = env.begin_ro_txn()?;
-        //let vers: String = txn.get(metadb, b"version")?.into();
-        let vers = txn.get(metadb, b"version")?;
-        let ts = txn.get(metadb, b"start_time")?;
-        println!("{:?} {:?}", vers, ts);
-        let vers = str::from_utf8(vers)?;
-        let ts = LittleEndian::read_u32(&ts);
-        println!("{} {}", vers, ts);
+        let version = database::metadb::read_version(metadb, &txn)?;
+        println!("version: {}", version);
     }
 
     Ok(())
