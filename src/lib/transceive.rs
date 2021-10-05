fde310e8c2828319dc5d0cfc5d02276f5782bf87
Tomas Krizek <tomas.krizek@nic.cz>
2021-10-05 14:49:47 +0200

    lib/transceive: transmit single query

--- src/lib/transceive.rs
+++ src/lib/transceive.rs
@@ -1,6 +1,7 @@
 /// Module for asynchronously transmitting queries.
 use async_std::{
     prelude::*,
+    io,
     task,
     net::{ SocketAddr, ToSocketAddrs, UdpSocket },
 };
@@ -10,7 +11,7 @@ use crate::{
     config::ServerConfig,
     database::queriesdb::Query,
 };
-use std::time::Duration;
+use std::time::{ Duration, Instant };
 
 type Sender<T> = mpsc::UnboundedSender<T>;
 type Receiver<T> = mpsc::UnboundedReceiver<T>;
@@ -31,17 +32,37 @@ enum Response {
 }
 
 async fn transmit_query(
-    query: Query,
+    query: Query,  // TODO maybe wire is enough?
     addrs: Vec<SocketAddr>,
     timeout: Duration,
     mut sink: Sender<Vec<Response>>
-) {
+) -> Result<(), io::Error> {
     // start timer
     // https://docs.rs/async-std/1.9.0/async_std/io/fn.timeout.html
     // send to each server (should be pretty much instant)
     // wait for all answers
     // push to sink
-    sink.send(vec![]).await;
+    let query = query.wire;
+
+    let addr = addrs[0]; // TODO
+    let reply = io::timeout(timeout, async {
+        let socket = UdpSocket::bind("0.0.0.0:0").await?;
+        socket.connect(&addr).await?;
+        socket.send(&query).await?;
+        let since = Instant::now();
+        let mut buf = vec![0; 64 * 1024];
+        let n = socket.recv(&mut buf).await?;
+        Ok(Response::Data {
+            delay: since.elapsed(),
+            wire: buf[0..n].to_vec(),
+        })
+    }).await;
+
+    match reply {  // TODO error handling for channel?
+        Ok(reply) => sink.send(vec![reply]).await,
+        Err(_) => sink.send(vec![Response::Timeout]).await,
+    };
+    Ok(())
 }
 
 async fn recv_loop(replies: Receiver<Vec<Response>>) {}
@@ -61,8 +82,8 @@ mod tests {
             let sock = UdpSocket::bind(addr).await.unwrap();
             let addr = sock.local_addr().unwrap();
             let (sender, mut receiver) = mpsc::unbounded();
-            transmit_query(query, vec![addr], Duration::from_millis(100), sender).await;
-            assert_eq!(receiver.next().await, Some(vec![]));
+            let res = transmit_query(query, vec![addr], Duration::from_millis(100), sender).await.unwrap();
+            assert_eq!(receiver.next().await, Some(vec![Response::Timeout]));
             assert_eq!(receiver.next().await, None);
         }).await;
         Ok(())
